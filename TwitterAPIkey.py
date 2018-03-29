73f9cea5284c1eddeda91b2d05c48f139e93e678
wyy0920 <wyy0920@gmail.com>
2018-03-29 10:15:41 -0700

    added twitter data

---
+++ TwitterAPIkey.py
@@ -0,0 +1,6 @@
+
+# Twitter API Keys
+consumer_key = "xN9z1haRcp679OVkxgdkhHXkb"
+consumer_secret = "z1RlMdLoeJprX4Vk4wff2A5mSxHBma7RVPZHvG6O7f0jjqQ0a3"
+access_token = "978841220767137793-k3M2C3XhcuGoB6rHtxyS4P9vbjLFCDj"
+access_token_secret = "DIWdqrMkYTN1TVh4MilE9a0NnNWwFzOIQxFveZ2de7j2x"
